47016a198b8b97b6df108a7167872d3b5fae6556
fireyyouth <fireyyouth@gmail.com>
2024-01-21 06:34:27 -0500

    fix model

--- app/models.py
+++ app/models.py
@@ -2,5 +2,5 @@ from django.db import models
 
 # Create your models here.
 class Preview(models.Model):
-    title = models.CharField()
-    img = models.CharField()
+    title = models.CharField(max_length=128)
+    img = models.CharField(max_length=128)
