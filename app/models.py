c137bb7efde8163be0483b841609bcca42b4d8d7
fireyyouth <fireyyouth@gmail.com>
2024-01-21 07:51:23 -0500

    allow long uri in PreviewModel

--- app/models.py
+++ app/models.py
@@ -2,5 +2,5 @@ from django.db import models
 
 # Create your models here.
 class Preview(models.Model):
-    title = models.CharField(max_length=128)
-    img = models.CharField(max_length=128)
+    title = models.CharField(max_length=512)
+    img = models.CharField(max_length=512)
