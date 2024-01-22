53be5824370daee0c597d18430fec30bb029746c
fireyyouth <fireyyouth@gmail.com>
2024-01-22 02:15:04 -0500

    add title_tsvector field to model

--- app/models.py
+++ app/models.py
@@ -1,6 +1,10 @@
 from django.db import models
+from django.contrib.postgres.search import SearchVectorField
 
 # Create your models here.
 class Preview(models.Model):
     title = models.CharField(max_length=512)
+    title_tsvector = SearchVectorField(null=True)
     img = models.CharField(max_length=512)
+
+
