1beafd931928bd66fa636cf78b2857526ba2407e
fireyyouth <fireyyouth@gmail.com>
2024-01-22 02:15:50 -0500

    add api for search

--- app/views.py
+++ app/views.py
@@ -1,9 +1,16 @@
 from django.http import JsonResponse
 from .models import Preview
 from app.serializers import PreviewSerializer
+from django.contrib.postgres.search import SearchQuery
 
 # Create your views here.
 
 def all(request):
     serializer = PreviewSerializer(Preview.objects.all(), many=True)
+    return JsonResponse(serializer.data, safe=False)
+
+# ref: https://zhuanlan.zhihu.com/p/523233840
+def search(request, keywords):
+    matched_objects = Preview.objects.filter(title_tsvector=SearchQuery(keywords))
+    serializer = PreviewSerializer(matched_objects, many=True)
     return JsonResponse(serializer.data, safe=False)
--- xigua/urls.py
+++ xigua/urls.py
@@ -20,5 +20,6 @@ from app import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/preview/', views.all)
+    path('api/preview/', views.all),
+    path('api/search/<keywords>', views.search)
 ]
